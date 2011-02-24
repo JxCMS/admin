b97cddad4a2dbf3fefaf07df44766cf23c843497
Jonathan Bomgardner <jonlb@comcast.net>
2011-02-24 15:02:17 -0800

    initial commit

---
+++ admin.js
@@ -0,0 +1,10 @@
+/**
+ * The base of the admin module. It provides the basic services for the admin
+ * area of any domain/subdomain in the system
+ */
+
+
+
+exports.init = function(db,router) {
+    return true;
+}
---
+++ controllers/admin.js
@@ -0,0 +1,37 @@
+/**
+ * admin controller
+ */
+
+/**
+ * Dependencies
+ */
+
+var Controller_Main = require('controller').Controller_Main;
+
+
+(function(){
+
+    exports = new Class({
+
+        Extends: Controller_Main,
+
+
+        before: function(){
+
+        },
+
+        after: function(){
+
+        },
+
+        index_action: function() {
+            
+        }
+
+    });
+
+    exports.routes = [
+        ['loader','GET /admin/(\\w+)/(\\d+)$', ['action','id'], ['admin/controllers/admin','index']]
+    ]
+
+})();
